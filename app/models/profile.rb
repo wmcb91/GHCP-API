0d9f4ad4f30749d27f4b0ecb203b1a8a07bac66a
wmcb91 <wmcb91@gmail.com>
2016-09-29 21:47:55 -0400

    Full association test for model Profile
    Full set of tests for association in the Article model.  Includes
    relationship with rounds which has yet to be created.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,7 @@
 class Profile < ActiveRecord::Base
+  validates :given_name, presence: true
+  validates :surname, presence: true
+  validates :dob, presence: true
+  validates :state, presence: true
+  has_many :rounds, inverse_of: :profile, dependent: :destroy
 end
---
+++ spec/models/profiles_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe Profile do
+  it 'is a profile' do
+    expect(Profile.new).to be_a(Profile)
+  end
+
+  describe 'validations' do
+    def valid_params
+      { given_name: 'Dround',
+        surname: 'TheMan',
+        dob: '1992-01-02',
+        state: 'MA' }
+    end
+
+    it 'validates the presence of an profile\'s given_name,
+    surname, dob, and state' do
+      expect(Profile.create(valid_params)).to be_valid
+    end
+
+    describe 'given_name' do
+      it 'is invalid without surname' do
+        invalid_params = valid_params.select { |key, _| key == :given_name }
+        expect(Profile.create(invalid_params)).to be_invalid
+      end
+    end
+
+    describe 'surname' do
+      it 'is invalid without given_name' do
+        invalid_params = valid_params.select { |key, _| key == :surname }
+        expect(Profile.create(invalid_params)).to be_invalid
+      end
+    end
+
+    describe 'surname' do
+      it 'is invalid without given_name' do
+        invalid_params = valid_params.select { |key, _| key == :dob }
+        expect(Profile.create(invalid_params)).to be_invalid
+      end
+    end
+
+    describe 'surname' do
+      it 'is invalid without given_name' do
+        invalid_params = valid_params.select { |key, _| key == :state }
+        expect(Profile.create(invalid_params)).to be_invalid
+      end
+    end
+  end
+
+  describe 'rounds association' do
+    def rounds_association
+      described_class.reflect_on_association(:rounds)
+    end
+
+    it 'has the name rounds' do
+      expect(rounds_association).to_not be_nil
+      expect(rounds_association.name).to eq(:rounds)
+    end
+
+    it 'has a set inverse of record' do
+      expect(rounds_association.options[:inverse_of]).to_not be_nil
+      expect(rounds_association.options[:inverse_of]).to eq(:profile)
+    end
+
+    it 'deletes associated rounds when destroyed' do
+      expect(rounds_association.options[:dependent]).to eq(:destroy)
+    end
+  end
+end
