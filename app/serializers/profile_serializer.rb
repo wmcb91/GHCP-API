f2658989fceb9dfe5294b07cc9a547a192f4658b
wmcb91 <wmcb91@gmail.com>
2016-09-30 13:49:27 -0400

    Improve serializers and examples
    Show better data by adding user email to profiles and showing the
    associated profiles when looking at a user.

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,5 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes :id, :given_name, :surname, :dob, :state, :rounds
+  belongs_to :user
   has_many :rounds
 end
--- db/examples.rb
+++ db/examples.rb
@@ -9,7 +9,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-%w(antony jeff matt jason).each do |name|
+%w(antony jeff matt tiger).each do |name|
   email = "#{name}@#{name}.com"
   next if User.exists? email: email
   User.create!(email: email,
@@ -18,54 +18,74 @@
 end
 
 Profile.create([
-                 { given_name: 'alice',
-                   surname: 'cooper',
+                 { given_name: 'Antony',
+                   surname: 'Cranks',
                    dob: '1992-01-02',
-                   state: 'massachusetts' },
-                 { given_name: 'wilbo',
-                   surname: 'cranks',
+                   user_id: 1,
+                   state: 'Massachusetts' },
+                 { given_name: 'Wilbo',
+                   surname: 'Cranks',
                    dob: '1993-01-02',
-                   state: 'florida' },
-                 { given_name: 'tiger',
-                   surname: 'woods',
+                   user_id: 1,
+                   state: 'Florida' },
+                 { given_name: 'Tiger',
+                   surname: 'Woods',
                    dob: '1977-01-02',
-                   state: 'california' }
+                   user_id: 4,
+                   state: 'California' },
+                 { given_name: 'Jeff',
+                   surname: 'Bridges',
+                   dob: '1967-01-02',
+                   user_id: 2,
+                   state: 'California' },
+                 { given_name: 'Matt',
+                   surname: 'Damon',
+                   dob: '1979-01-02',
+                   user_id: 3,
+                   state: 'Massachusetts' }
                ])
 
 Round.create([
-               { course: 'butter brook',
+               { course: 'Butter Brook',
                  date: '2016-09-29',
                  rating: 72.5,
                  slope: 133,
                  par: 72,
                  score: 90,
                  profile_id: 1 },
-               { course: 'butter brook',
+               { course: 'Butter Brook',
                  date: '2016-09-29',
                  rating: 72.5,
                  slope: 133,
                  par: 72,
                  score: 87,
                  profile_id: 1 },
-               { course: 'butter brook',
+               { course: 'Butter Brook',
                  date: '2016-09-29',
                  rating: 72.5,
                  slope: 133,
                  par: 72,
                  score: 86,
                  profile_id: 1 },
-               { course: 'butter brook',
+               { course: 'Butter Brook',
                  date: '2016-09-29',
                  rating: 72.5,
                  slope: 133,
                  par: 72,
                  score: 66,
                  profile_id: 3 },
-               { course: 'butter brook',
+               { course: 'Butter Brook',
                  date: '2016-09-29',
                  rating: 72.5,
                  slope: 133,
                  par: 72,
                  score: 68,
-                 profile_id: 3 }
+                 profile_id: 3 },
+               { course: 'Butter Brook',
+                 date: '2016-09-29',
+                 rating: 72.5,
+                 slope: 133,
+                 par: 72,
+                 score: 69,
+                 profile_id: 2 }
              ])
