9a1fc6c3ed4f59e19d7967e4db8147ce879be3fb
wmcb91 <wmcb91@gmail.com>
2016-11-16 22:10:48 -0500

    Update validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,6 @@ class Profile < ActiveRecord::Base
   validates :given_name, presence: true
   validates :surname, presence: true
   validates :home_course, presence: true
-  validates :home_course_slope
   has_many :rounds, dependent: :destroy
   belongs_to :user
 end
